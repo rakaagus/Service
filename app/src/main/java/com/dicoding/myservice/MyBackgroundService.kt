ec980ab55a0878f8db43090acd503abdfd423816
ArtForYou <kakakunyukya@gmail.com>
2023-09-29 16:08:48 +0700

    Initial Commit

---
+++ app/src/main/java/com/dicoding/myservice/MyBackgroundService.kt
@@ -0,0 +1,44 @@
+package com.dicoding.myservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyBackgroundService : Service() {
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    override fun onBind(intent: Intent): IBinder {
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "Service dijalankan.....")
+        serviceScope.launch {
+            for (i in 1..50){
+                delay(1000)
+                Log.d(TAG, "do something $i")
+            }
+            stopSelf()
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+        Log.d(TAG, "onDestroy: Service Di hentikan")
+    }
+
+    companion object{
+        internal val TAG = MyBackgroundService::class.java.simpleName
+    }
+}
