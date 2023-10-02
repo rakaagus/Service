b1b1788dc6f82c0cef2178fc7f080e31e4df7493
ArtForYou <kakakunyukya@gmail.com>
2023-10-02 13:53:56 +0700

    Update BoundService

--- app/src/main/java/com/dicoding/myservice/MainActivity.kt
+++ app/src/main/java/com/dicoding/myservice/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.dicoding.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.dicoding.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var boundStatus = false
+    private lateinit var boundService: MyBoundService
+
+    private val connection = object : ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            boundService = myBinder.getService
+            boundStatus = true
+            getNumberFromService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundStatus = false
+        }
+
+    }
+
+    private fun getNumberFromService() {
+        boundService.numberLiveData.observe(this){ number->
+            binding.tvBoundServiceNumber.text = number.toString()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +58,22 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnStopForegroundService.setOnClickListener {
             stopService(foregroundServiceIntent)
+        }
+
+        val boundServiceIntent = Intent(this@MainActivity, MyBoundService::class.java)
+        binding.btnStartBoundService.setOnClickListener {
+            bindService(boundServiceIntent, connection, BIND_AUTO_CREATE)
+        }
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(connection)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(boundStatus){
+            unbindService(connection)
+            boundStatus = false
         }
     }
 }
---
+++ app/src/main/java/com/dicoding/myservice/MyBoundService.kt
@@ -0,0 +1,58 @@
+package com.dicoding.myservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyBoundService : Service() {
+
+    private var binder = MyBinder()
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+    val numberLiveData: MutableLiveData<Int> = MutableLiveData()
+
+    override fun onBind(intent: Intent): IBinder {
+        Log.d(TAG, "onBind: ")
+        serviceScope.launch {
+            for (i in 1..50){
+                delay(1000)
+                Log.d(TAG, "do something $i")
+                numberLiveData.postValue(i)
+            }
+        Log.d(TAG, "Service Dihentikan")
+        }
+        return binder
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d(TAG, "onUnbind: ")
+        serviceJob.cancel()
+        return super.onUnbind(intent)
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+        Log.d(TAG, "onRebind: ")
+    }
+
+    internal inner class MyBinder: Binder(){
+        val getService: MyBoundService = this@MyBoundService
+    }
+
+    companion object{
+        private val TAG = MyBoundService::class.java.simpleName
+    }
+}
